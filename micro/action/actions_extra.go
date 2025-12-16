c645cbdd207492900b7e784fe991360ec5d7fd57
Simon Edwards <simon@simonzone.com>
2025-12-16 19:13:19 +0100

    Add support for `Cursor.TransformSelection()`

---
+++ micro/action/actions_extra.go
@@ -0,0 +1,39 @@
+package action
+
+// Extra actions which are not from micro
+
+func (h *BufPane) TransformSelection(transformFunc func([]string) []string) {
+	cursors := h.Buf.GetCursors()
+	for _, c := range cursors {
+		lines := c.GetSelectionLines()
+
+		strLines := make([]string, len(lines))
+		for i, line := range lines {
+			strLines[i] = string(line)
+		}
+
+		transformedLines := transformFunc(strLines)
+
+		startLoc := c.CurSelection[0]
+		endLoc := c.CurSelection[1]
+		if startLoc.GreaterThan(endLoc) {
+			startLoc = endLoc
+		}
+
+		c.DeleteSelection()
+
+		insertLoc := startLoc
+		endLoc = startLoc
+		for i, tline := range transformedLines {
+			if i != 0 {
+				h.Buf.Insert(insertLoc, "\n")
+				insertLoc.Y++
+				insertLoc.X = 0
+			}
+			h.Buf.Insert(insertLoc, tline)
+			insertLoc.X += len(tline)
+		}
+		c.SetSelectionStart(startLoc)
+		c.SetSelectionEnd(insertLoc)
+	}
+}
